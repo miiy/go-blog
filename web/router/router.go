f89d75c8c468512d921d7741f40b09d55de5aa9c
miiy <miiy@users.noreply.github.com>
2022-03-16 23:01:52 +0800

    Update

--- web/router/router.go
+++ web/router/router.go
@@ -23,43 +23,55 @@ func RegisterRouter(r *gin.Engine) {
 	r.StaticFS("/assets", http.FS(assets.FS))
 
 	// favicon
-	r.GET("/favicon.ico", func(c *gin.Context) {
-		file, _ := assets.FS.ReadFile("favicon.ico")
-		c.Data(http.StatusOK, "image/x-icon", file)
-	})
+	faviconHandler := func(c *gin.Context) {
+		c.FileFromFS("favicon.ico", http.FS(assets.FS))
+	}
+	r.HEAD("/favicon.ico", faviconHandler)
+	r.GET("/favicon.ico", faviconHandler)
 
 	// uploads
 	r.Static("/uploads", "./storage/uploads")
 
-	// pages
-	r.GET("/pages/list", func(c *gin.Context) {
-		c.HTML(200, "pages/list",  gin.H{})
-
-	})
-	r.GET("/pages/detail", func(c *gin.Context) {
-		c.HTML(200, "pages/detail",  gin.H{})
-	})
 
 	// index
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "home/index",  gin.H{
+		c.HTML(200, "home/index", gin.H{
 			"PageTitle": "Home",
-			"Content": "Hello, world.",
-			"Header": "header.",
+			"Content":   "Hello, world.",
+			"Header":    "header.",
 		})
 	})
 
+	// article
+	r.GET("/articles", func(c *gin.Context) {
+		c.HTML(200, "article/list", gin.H{
+			"PageTitle": "Article list",
+		})
+	})
+	r.GET("/articles/:id", func(c *gin.Context) {
+		c.HTML(200, "article/detail", gin.H{
+			"PageTitle": "Article detail",
+		})
+	})
 	// book
 	r.GET("/books", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "books",
+		c.HTML(200, "book/list", gin.H{
+			"PageTitle": "Book list",
 		})
 	})
 	// book detail
-	r.GET("/book/:id", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "books detial",
+	r.GET("/books/:id", func(c *gin.Context) {
+		c.HTML(200, "book/detail", gin.H{
+			"PageTitle": "Book detail",
 		})
+	})
+
+	// pages
+	r.GET("/pages/list", func(c *gin.Context) {
+		c.HTML(200, "pages/list", gin.H{})
+	})
+	r.GET("/pages/detail", func(c *gin.Context) {
+		c.HTML(200, "pages/detail", gin.H{})
 	})
 
 	// middleware
@@ -72,13 +84,17 @@ func createTemplateRender() (multitemplate.Renderer, error) {
 
 	layouts := []string{"layout/layout.html", "layout/header.html", "layout/footer.html"}
 	templatesMap := map[string][]string{
-		"home/index": {"home/index.html"},
-		"pages/list": {"pages/list.html"},
-		"pages/detail": {"pages/detail.html"},
+		"home/index":     {"home/index.html"},
+		"article/detail": {"article/detail.html"},
+		"article/list":   {"article/list.html"},
+		"book/detail":    {"book/detail.html"},
+		"book/list":      {"book/list.html"},
+		"pages/list":     {"pages/list.html"},
+		"pages/detail":   {"pages/detail.html"},
 	}
 
 	for name, tps := range templatesMap {
-		s :=  append(layouts, tps...)
+		s := append(layouts, tps...)
 		t, err := template.ParseFS(templates.FS, s...)
 		if err != nil {
 			return nil, err
